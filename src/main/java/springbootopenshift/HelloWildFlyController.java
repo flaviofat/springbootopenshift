46afd3341cfa6e8a728e1b1b875cf64046fe851c
Flavio Teixeira <flavio.augustot@gmail.com>
2017-11-05 01:19:06 -0200

    started

---
+++ src/main/java/springbootopenshift/HelloWildFlyController.java
@@ -0,0 +1,14 @@
+package springbootopenshift;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWildFlyController {
+
+
+    @RequestMapping("hello")
+    public String sayHello(){
+        return ("Hello World, i'm alive");
+    }
+}
